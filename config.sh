20de6cf4d9302235232bbeee2683599ae82d7267
Alessio Sanfratello <sanfra90@gmail.com>
2020-09-17 00:26:58 +0200

    Removing python 2.7 and macOS 10.6 builds, fixed test

--- config.sh
+++ config.sh
@@ -10,5 +10,5 @@ function pre_build {
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    python -c 'import sys; import yourpackage; sys.exit(yourpackage.test())'
+    python -c 'import dlib'
 }
