e91984fdd1ac60e4a4d4744bb1c9233d48641bb8
Alessio Sanfratello <sanfra90@gmail.com>
2020-09-17 23:23:44 +0200

    Fix regex

--- config.sh
+++ config.sh
@@ -4,8 +4,8 @@
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    sed -i '' 's/name='"'"'dlib'"'"'/name='"'"'dlib-bin'"'"'/' dlib/setup.py
-    sed -i '' 's/url='"'"'https:\/\/github\.com\/davisking\/dlib'"'"'/url='"'"'https:\/\/github\.com\/alesanfra\/dlib-wheels'"'"'/' dlib/setup.py
+    sed -i'' -e 's/name='"'"'dlib'"'"'/name='"'"'dlib-bin'"'"'/' dlib/setup.py
+    sed -i'' -e 's/url='"'"'https:\/\/github\.com\/davisking\/dlib'"'"'/url='"'"'https:\/\/github\.com\/alesanfra\/dlib-wheels'"'"'/' dlib/setup.py
 }
 
 function run_tests {
